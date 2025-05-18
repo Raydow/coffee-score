c4feea7b20870b2b38135950aa24853a41feb845
Tiago Pereira <tiago.pereira@totvs.com.br>
2025-05-18 08:50:53 -0300

    WIP login and new account routes and layout.

---
+++ efit-frontend/src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+  import Router from 'svelte-spa-router';
+
+  import Home from './routes/Home.svelte';
+  import About from './routes/About.svelte';
+  import Register from './routes/Register.svelte';
+  import NotFound from './routes/NotFound.svelte';
+
+  const routes = {
+    '/': Home,
+    '/about': About,
+    '/register': Register,
+    '*': NotFound
+  };
+</script>
+
+<nav>
+  <a href="#/">Home</a>
+  <a href="#/about">Sobre</a>
+</nav>
+
+<main>
+  <Router {routes} />
+</main>
---
+++ efit-frontend/src/routes/Login.svelte
@@ -0,0 +1,25 @@
+<script>
+    import goto from 'svelte-spa-router'
+  
+    let email = ''
+    let password = ''
+  
+    function login() {
+      // Simula login e navega para Home
+      console.log('Login:', email, password)
+      goto('/home')
+    }
+  
+    function goToRegister() {
+      goto('/register')
+    }
+  </script>
+  
+  <div>
+    <h2>Login</h2>
+    <input type="email" placeholder="Email" bind:value={email} />
+    <input type="password" placeholder="Senha" bind:value={password} />
+    <button on:click={login}>Entrar</button>
+    <button on:click={goToRegister}>Criar Conta</button>
+  </div>
+  
